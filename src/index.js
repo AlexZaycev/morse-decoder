92a7df97830739884ff256025c6bde8900da8675
Alex Zaycev <lexx.zaycev@gmail.com>
2022-08-12 22:12:57 +0300

    complete morse decoder

--- src/index.js
+++ src/index.js
@@ -38,8 +38,21 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result = '';
+
+    for(let i = 0; i < expr.length; i += 10) {
+      const letter = expr.slice(0 + i, 10 + i);
+
+      if(isNaN(letter)) {
+        result += ' ';
+      } else {
+        result += MORSE_TABLE[letter.split('00').join('').split('10').join('.').split('11').join('-')];
+      }
+    }
+
+    return result;
 }
+
 
 module.exports = {
     decode
